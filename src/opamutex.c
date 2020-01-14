92604aaddf4db4bba23a6b6e1eab68aae61e6041
opatomic <j@opatomic.com>
2020-01-13 19:21:02 -0500

    opamutex: use function instead of macro to detect/log err

--- src/opamutex.c
+++ src/opamutex.c
@@ -33,18 +33,23 @@ void opamutexUnlock(opamutex* m) {
 
 #else
 
-#define PANICIFERR(e) (__extension__({ __typeof__(e) _e = (e); if (_e) {LOGSYSERR(_e); OPAPANIC("panic due to mutex error");} }))
+static void opamutexPanicIfErr(int err) {
+	if (err) {
+		LOGSYSERR(err);
+		OPAPANIC("panic due to mutex error");
+	}
+}
 
 void opamutexInit(opamutex* m) {
-	PANICIFERR(pthread_mutex_init(m, NULL));
+	opamutexPanicIfErr(pthread_mutex_init(m, NULL));
 }
 
 void opamutexDestroy(opamutex* m) {
-	PANICIFERR(pthread_mutex_destroy(m));
+	opamutexPanicIfErr(pthread_mutex_destroy(m));
 }
 
 void opamutexLock(opamutex* m) {
-	PANICIFERR(pthread_mutex_lock(m));
+	opamutexPanicIfErr(pthread_mutex_lock(m));
 }
 
 int opamutexTryLock(opamutex* m) {
@@ -52,13 +57,13 @@ int opamutexTryLock(opamutex* m) {
 	if (res == 0) {
 		return 1;
 	} else if (res != EBUSY) {
-		PANICIFERR(res);
+		opamutexPanicIfErr(res);
 	}
 	return 0;
 }
 
 void opamutexUnlock(opamutex* m) {
-	PANICIFERR(pthread_mutex_unlock(m));
+	opamutexPanicIfErr(pthread_mutex_unlock(m));
 }
 
 #endif
