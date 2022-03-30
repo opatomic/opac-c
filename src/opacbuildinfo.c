43537c4204a17b0e61116cea26e5efe24bc03c18
opatomic <j@opatomic.com>
2022-03-30 11:14:09 -0400

    don't quote OPAC_VERSION definition

--- src/opacbuildinfo.c
+++ src/opacbuildinfo.c
@@ -6,8 +6,11 @@
 #include "opac.h"
 #include "opabigint.h"
 
+#define PPSTR(a) #a
+#define PPXSTR(a) PPSTR(a)
+
 #ifndef OPAC_VERSION
-#define OPAC_VERSION "0.0.0-dev"
+#define OPAC_VERSION 0.0.0-dev
 #endif
 
 #ifdef OPA_NOTHREADS
@@ -20,7 +23,7 @@
 //    see: gmp_version __GNU_MP_VERSION __GNU_MP_VERSION_MINOR __GNU_MP_VERSION_PATCH
 
 const opacBuildInfo BUILDINFO = {
-	OPAC_VERSION, OPAC_BITHREADS, OPABIGINT_LIB_NAME
+	PPXSTR(OPAC_VERSION), OPAC_BITHREADS, OPABIGINT_LIB_NAME
 };
 
 const opacBuildInfo* opacGetBuildInfo(void) {
